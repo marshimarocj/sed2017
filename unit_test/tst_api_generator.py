254f92206346c1c205891dd0100f35568e399da0
chenjia <marshimarocj@gmail.com>
2017-07-18 09:00:41 -0400

    sync

--- unit_test/tst_api_generator.py
+++ unit_test/tst_api_generator.py
@@ -34,8 +34,8 @@ def tst_c3d_toi():
 
   cnt = 0
   for trackletid, fts in ft_in_track_generator:
-    # print trackletid, fts['frame'], fts['center'][0], fts['center'][1], fts['ft'].shape
-    print trackletid, fts
+    print trackletid, fts['frame'], fts['center'], fts['ft'].shape
+    # print trackletid, fts
     cnt += 1
     if cnt == 100:
       break
