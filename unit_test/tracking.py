bb0e9daa5183628b41be5a57a9a4c229425584f6
chenjia <marshimarocj@gmail.com>
2017-07-20 12:24:26 -0400

    sync

--- unit_test/tracking.py
+++ unit_test/tracking.py
@@ -11,7 +11,7 @@ def generate_script():
   root_dir = '/usr0/home/jiac/data/sed/tst2017' # aladdin3
   video_lst_file = os.path.join(root_dir, '2017.refined.lst')
   preprocess_dir = os.path.join(root_dir, 'dev09_preprocess')
-  out_dir = os.path.join(root_dir, 'script')
+  script_dir = os.path.join(root_dir, 'script')
 
   image_name = 'diva_tracking:v1'
 
@@ -19,7 +19,7 @@ def generate_script():
     for line in f:
       videoname = line.strip()
       clip_lst_file = os.path.join(preprocess_dir, videoname, 'clip_6000_100.lst')
-      out_file = os.path.join(out_dir, videoname + '.sh')
+      out_file = os.path.join(script_dir, videoname + '.sh')
       if not os.path.exists(clip_lst_file):
         # print videoname
         continue
