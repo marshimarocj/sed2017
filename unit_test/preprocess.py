1f45971ccc30ab5cf339c4b42714578153255ce8
chenjia <marshimarocj@gmail.com>
2017-07-20 20:49:40 -0400

    sync

--- unit_test/preprocess.py
+++ unit_test/preprocess.py
@@ -41,22 +41,20 @@ def generate_08_script():
 
 
 def missing_videos_in_preprocess():
-  root_dir = '/home/jiac/data/sed/video' # xiaojun
-  lst_file = os.path.join(root_dir, 'video2017.lst')
+  root_dir = '/usr0/home/jiac/data/sed/tst2017' # aladdin3
+  lst_file = os.path.join(root_dir, 'refined.2017.lst')
   preprocess_root_dir = os.path.join(root_dir, 'dev09_preprocess')
+  out_file = os.path.join(root_dir, 'dev09_preprocess.short.lst')
 
-  with open(lst_file) as f:
+  with open(lst_file) as f, open(out_file, 'w') as fout:
     for line in f:
-      line = line.strip()
-      pos = line.find('.')
-      name = line[:pos]
+      name = line.strip()
       preprocess_dir = os.path.join(preprocess_root_dir, name)
-      if not os.path.exists(preprocess_dir):
-        print name
-
-
-def get_25fps_lst_from_5fps_lst():
-  root_dir = '' # gpu1
+      lst_file = os.path.join(preprocess_dir, 'frame_5.lst')
+      # if not os.path.exists(preprocess_dir):
+      #   print name
+      if not os.path.exists(lst_file):
+        fout.write(name + '\n')
 
 
 if __name__ == '__main__':
