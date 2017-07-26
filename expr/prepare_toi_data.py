8e3904e00f32ffa428d55c8f5650088374a3794c
chenjia <marshimarocj@gmail.com>
2017-07-26 11:39:02 -0400

    sync

--- expr/prepare_toi_data.py
+++ expr/prepare_toi_data.py
@@ -182,8 +182,8 @@ def prepare_neg_c3d():
 
 
 def generate_script():
-  root_dir = '/data1/jiac/sed' # uranus
-  # root_dir = '/home/jiac/data2/sed' # gpu9
+  # root_dir = '/data1/jiac/sed' # uranus
+  root_dir = '/home/jiac/data2/sed' # gpu9
   lst_files = [
     os.path.join(root_dir, 'dev08-1.lst'),
     os.path.join(root_dir, 'eev08-1.lst'),
@@ -265,9 +265,62 @@ def prepare_pos_vgg19():
     np.savez_compressed(out_file, fts=fts, frames=frames, centers=centers, ids=ids)
 
 
+def prepare_neg_vgg19():
+  root_dir = '/home/jiac/data2/sed' # gpu9
+  label_dir = os.path.join(root_dir, 'pseudo_label')
+  track_dir = os.path.join(root_dir, 'tracking')
+  ft_root_dir = os.path.join(root_dir, 'vgg19_pool5_fullres')
+  out_dir = os.path.join(ft_root_dir, 'track_group')
+
+  track_lens = [25, 50]
+  neg_split = 0
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('name')
+  args = parser.parse_args()
+  name = args.name
+
+  vgg_centers = api.db.get_vgg19_centers()
+
+  for track_len in track_lens:
+    label_file = os.path.join(label_dir, '%s.%d.forward.backward.square.0.50.neg.%d'%(name, track_len, neg_split))
+    neg_trackids = []
+    with open(label_file) as f:
+      for line in f:
+        line = line.strip()
+        neg_trackids.append(int(line))
+
+    db_file = os.path.join(track_dir,'%s.%d.forward.backward.square.npz'%(name, track_len))
+    track_db = api.db.TrackDb()
+    track_db.load(db_file, neg_trackids)
+
+    ft_dir = os.path.join(ft_root_dir, name)
+    vgg_db = api.db.VGG19FtDb(ft_dir)
+
+    neg_vgg_in_track_generator = api.generator.crop_instant_ft_in_track(
+      track_db, vgg_db, vgg_centers)
+    fts = []
+    frames = []
+    centers = []
+    ids = []
+    for ft_in_track in neg_vgg_in_track_generator:
+      num = len(ft_in_track.frames)
+      fts.append(ft_in_track.fts)
+      frames.extend(ft_in_track.frames)
+      centers.append(ft_in_track.centers)
+      ids.extend(num*[ft_in_track.id])
+
+    fts = np.concatenate(fts, 0)
+    frames = np.array(frames, dtype=np.int32)
+    centers = np.concatenate(centers, 0)
+    ids = np.array(ids, dtype=np.int32)
+    out_file = os.path.join(out_dir, '%s.%d.forward.backward.square.neg.0.50.%d.npz'%(name, track_len, neg_split))
+    np.savez_compressed(out_file, fts=fts, frames=frames, centers=centers, ids=ids)
+
+
 if __name__ == '__main__':
   # prepare_pos_c3d()
-  # generate_script()
+  generate_script()
   # prepare_pos_vgg19()
   # shuffle_neg()
-  prepare_neg_c3d()
+  # prepare_neg_c3d()
