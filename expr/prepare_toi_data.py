41b1bf310f2190322a149d8e4562c464cd7f2b2f
chenjia <marshimarocj@gmail.com>
2017-07-26 11:28:36 -0400

    sync

--- expr/prepare_toi_data.py
+++ expr/prepare_toi_data.py
@@ -267,7 +267,7 @@ def prepare_pos_vgg19():
 
 if __name__ == '__main__':
   # prepare_pos_c3d()
-  generate_script()
+  # generate_script()
   # prepare_pos_vgg19()
   # shuffle_neg()
-  # prepare_neg_c3d()
+  prepare_neg_c3d()
