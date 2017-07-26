961f41ebaad2b8125b7d32bc4e9e06e31f153c65
chenjia <marshimarocj@gmail.com>
2017-07-26 02:07:26 -0400

    sync

--- api/generator.py
+++ api/generator.py
@@ -205,6 +205,7 @@ def crop_clip_in_track(clipdb, trackdb):
           continue
 
         box = track.track[frame-track.start_frame]
+        box = box.astype(np.int32)
 
         if id not in cache:
           cache[id] = []
@@ -216,8 +217,6 @@ def crop_clip_in_track(clipdb, trackdb):
         rend = img.shape[0] - box[1] if box[3] > img.shape[0] else box[3]-box[1]
         cstart = -box[0] if box[0] < 0 else 0
         cend = img.shape[1] - box[0] if box[2] > img.shape[1] else box[2]-box[0]
-        print box
-        print rstart, rend, cstart, cend
         _img[rstart:rend, cstart:cend] = img[max(box[1], 0):box[3], max(box[0], 0):box[2]]
 
         cache[id].append(_img)
