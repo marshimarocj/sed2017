9bc077b1a729fa9d54dd534d2f6d9479024498a0
chenjia <marshimarocj@gmail.com>
2017-07-26 11:27:00 -0400

    sync

--- expr/prepare_toi_data.py
+++ expr/prepare_toi_data.py
@@ -136,6 +136,7 @@ def prepare_neg_c3d():
   out_dir = os.path.join(root_dir, 'c3d', 'track_group')
 
   track_lens = [25, 50]
+  neg_split = 0
 
   parser = argparse.ArgumentParser()
   parser.add_argument('name')
@@ -145,24 +146,27 @@ def prepare_neg_c3d():
   c3d_centers = api.db.get_c3d_centers()
 
   for track_len in track_lens:
-    label_file = os.path.join(label_dir, '%s.%d.forward.backward.square.0.75.pos'%(name, track_len))
-    id2event = load_pos_track_label_file(label_file)
-    pos_trackids = id2event.keys()
+    label_file = os.path.join(label_dir, '%s.%d.forward.backward.square.0.50.neg.%d'%(name, track_len, neg_split))
+    neg_trackids = []
+    with open(label_file) as f:
+      for line in f:
+        line = line.strip()
+        neg_trackids.append(int(line))
 
     db_file = os.path.join(track_dir, '%s.%d.forward.backward.square.npz'%(name, track_len))
     track_db = api.db.TrackDb()
-    track_db.load(db_file, pos_trackids)
+    track_db.load(db_file, neg_trackids)
 
     ft_dir = os.path.join(ft_root_dir, name)
     c3d_db = api.db.C3DFtDb(ft_dir)
 
-    pos_c3d_in_track_generator = api.generator.crop_duration_ft_in_track(
+    neg_c3d_in_track_generator = api.generator.crop_duration_ft_in_track(
       track_db, c3d_db, c3d_centers, c3d_threshold_func)
     fts = []
     frames = []
     centers = []
     ids = []
-    for ft_in_track in pos_c3d_in_track_generator:
+    for ft_in_track in neg_c3d_in_track_generator:
       num = len(ft_in_track.frames)
       fts.append(ft_in_track.fts)
       frames.extend(ft_in_track.frames)
@@ -173,7 +177,7 @@ def prepare_neg_c3d():
     frames = np.array(frames, dtype=np.int32)
     centers = np.concatenate(centers, 0)
     ids = np.array(ids, dtype=np.int32)
-    out_file = os.path.join(out_dir, '%s.%d.forward.backward.square.pos.0.75.npz'%(name, track_len))
+    out_file = os.path.join(out_dir, '%s.%d.forward.backward.square.neg.0.50.%d.npz'%(name, track_len, neg_split))
     np.savez_compressed(out_file, fts=fts, frames=frames, centers=centers, ids=ids)
 
 
@@ -202,12 +206,12 @@ def generate_script():
 
   for i in range(0, num, gap):
     idx = i/gap
-    out_file = 'prepare_pos_data.%d.sh'%idx
+    out_file = 'prepare_toi_data.%d.sh'%idx
     with open(out_file, 'w') as fout:
       for j in range(i, min(i+gap, num)):
         name = names[j]
         cmd = [
-          'python', 'prepare_pos_data.py', name
+          'python', 'prepare_toi_data.py', name
         ]
         fout.write(' '.join(cmd) + '\n')
 
@@ -263,6 +267,7 @@ def prepare_pos_vgg19():
 
 if __name__ == '__main__':
   # prepare_pos_c3d()
-  # generate_script()
+  generate_script()
   # prepare_pos_vgg19()
-  shuffle_neg()
+  # shuffle_neg()
+  # prepare_neg_c3d()
