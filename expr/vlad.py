24764c32dd55a328f862249d96500e017fe9fae6
chenjia <marshimarocj@gmail.com>
2017-07-27 09:13:50 -0400

    sync

--- expr/vlad.py
+++ expr/vlad.py
@@ -18,7 +18,7 @@ def encode(fts, kmeans):
   centers = kmeans.cluster_centers_
   diffs = []
   for i in range(kmeans.n_clusters):
-    idx = np.nonzero(centers_idxs==i)[0]
+    idx = np.nonzero(center_idxs==i)[0]
     diff = fts[idx] - np.expand_dims(centers[i], 0)
     if diff.shape[0] > 0:
       diff = np.sum(diff, 0)
