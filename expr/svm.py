ddb6fb84d27e4cf0aa7b790e6e65c2c63899d5e0
chenjia <marshimarocj@gmail.com>
2017-07-28 05:50:49 -0400

    sync

--- expr/svm.py
+++ expr/svm.py
@@ -219,10 +219,14 @@ def train_model():
 
 
 def val_model():
-  root_dir = '/data1/jiac/sed' # uranus
-  pos_val_file = os.path.join(root_dir, 'expr', 'c3d', 'eev08.vlad.pos.npz')
-  neg_val_file = os.path.join(root_dir, 'expr', 'c3d', 'eev08.vlad.neg.5.npz')
-  model_file = os.path.join(root_dir, 'expr', 'c3d', 'svm.CellToEar.Embrace.Pointing.PersonRuns.pkl')
+  # root_dir = '/data1/jiac/sed' # uranus
+  # pos_val_file = os.path.join(root_dir, 'expr', 'c3d', 'eev08.vlad.pos.npz')
+  # neg_val_file = os.path.join(root_dir, 'expr', 'c3d', 'eev08.vlad.neg.5.npz')
+  # model_file = os.path.join(root_dir, 'expr', 'c3d', 'svm.CellToEar.Embrace.Pointing.PersonRuns.pkl')
+  root_dir = '/home/jiac/data2/sed' # gpu9
+  pos_val_file = os.path.join(root_dir, 'expr', 'vgg19', 'eev08.vlad.pos.npz')
+  neg_val_file = os.path.join(root_dir, 'expr', 'vgg19', 'eev08.vlad.neg.5.npz')
+  model_file = os.path.join(root_dir, 'expr', 'vgg19', 'svm.CellToEar.Embrace.Pointing.PersonRuns.pkl')
 
   with open(model_file) as f:
     model = cPickle.load(f)
@@ -257,5 +261,5 @@ if __name__ == '__main__':
   # prepare_trn_tst_pos_data()
   # prepare_trn_tst_neg_data()
   # prepare_trn_data()
-  train_model()
-  # val_model()
+  # train_model()
+  val_model()
