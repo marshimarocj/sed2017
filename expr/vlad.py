e886bd47ebe6b7a70f4215fcbb7056cb17e92fa5
chenjia <marshimarocj@gmail.com>
2017-07-27 20:53:05 -0400

    sync

--- expr/vlad.py
+++ expr/vlad.py
@@ -35,10 +35,12 @@ def encode(fts, kmeans):
 '''expr
 '''
 def sample_data_for_center():
-  root_dir = '/data1/jiac/sed' # uranus
-  ft_root_dir = os.path.join(root_dir, 'c3d')
+  # root_dir = '/data1/jiac/sed' # uranus
+  # ft_root_dir = os.path.join(root_dir, 'c3d')
   # root_dir = '/home/jiac/data2/sed' # gpu9
   # ft_root_dir = os.path.join(root_dir, 'vgg19_pool5_fullres')
+  root_dir = '/home/jiac/data/sed' # xiaojun
+  ft_root_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_5frame')
   num_sample = 10000
   out_file = os.path.join(ft_root_dir, 'sample.%d.npy'%num_sample)
 
@@ -53,18 +55,57 @@ def sample_data_for_center():
 
   for name in names:
     ft_dir = os.path.join(ft_root_dir, name)
-    c3d_db = api.db.C3DFtDb(ft_dir)
-    # vgg_db = api.db.VGG19FtDb(ft_dir)
+    # c3d_db = api.db.C3DFtDb(ft_dir)
+    vgg_db = api.db.VGG19FtDb(ft_dir)
     print name
 
-    for chunk in c3d_db.chunks:
-    # for chunk in vgg_db.chunks:
+    # for chunk in c3d_db.chunks:
+    for chunk in vgg_db.chunks:
       print chunk
-      fts = c3d_db.load_chunk(chunk)
-      # fts = vgg_db.load_chunk(chunk)
+      # fts = c3d_db.load_chunk(chunk)
+      fts = vgg_db.load_chunk(chunk)
       shape = fts.shape
       for i, j, k in itertools.product(range(shape[0]), range(shape[2]), range(shape[3])):
         rs.addData(np.array(fts[i, :, j, k]))
+      del fts
+
+  data = rs.pool
+  np.save(out_file, data)
+
+
+def sample_data_for_twostream_sync_center():
+  root_dir = '/home/jiac/data/sed' # xiaojun
+  flow_ft_root_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_5frame')
+  rgb_ft_root_dir = os.path.join(root_dir, 'vgg19_pool5_fullres')
+  num_sample = 10000
+  out_file = os.path.join(flow_ft_root_dir, 'sync.sample.%d.npy'%num_sample)
+
+  names = [
+    'LGW_20071101_E1_CAM1',
+    'LGW_20071106_E1_CAM2',
+    'LGW_20071107_E1_CAM3',
+    'LGW_20071108_E1_CAM5',
+  ]
+
+  rs = sample.ReservoirSampling(num_sample)
+
+  for name in names:
+    flow_ft_dir = os.path.join(flow_ft_root_dir, name)
+    flow_ft_db = apid.db.VGG19FtDb(flow_ft_dir) # it's a hack
+
+    rgb_ft_dir = os.path.join(rgb_ft_root_dir, name)
+    rgb_ft_db = api.db.VGG19FtDb(rgb_ft_dir)
+
+    print name
+
+    for chunk in rgb_ft_db.chunks:
+      print chunk
+      rgb_fts = rgb_ft_db.load_chunk(chunk)
+      flow_fts = flow_ft_db.load_chunk(chunk)
+      shape = rgb_fts.shape
+      for i, j, k in itertools.product(range(shape[0]), range(shape[2]), range(shape[3])):
+        descriptor = np.concatenate([rgb_fts[i, :, j, k], flow_fts[i, :, j, k]])
+        rs.addData(descriptor)
       del fts
 
   data = rs.pool
@@ -166,6 +207,7 @@ def encode_vlad():
 
 
 if __name__ == '__main__':
-  # sample_data_for_center()
+  sample_data_for_center()
+  # sample_data_for_twostream_sync_center()
   # cluster_centers()
-  encode_vlad()
+  # encode_vlad()
