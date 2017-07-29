7dd671a9f3ad857505d4a484645a39b3de985e54
chenjia <marshimarocj@gmail.com>
2017-07-29 10:39:22 -0400

    sync

--- expr/svm.py
+++ expr/svm.py
@@ -507,8 +507,8 @@ def val_model():
 if __name__ == '__main__':
   # prepare_trn_tst_pos_data()
   # sample_neg_ids()
-  prepare_trn_tst_neg_data()
-  # prepare_trn_data()
+  # prepare_trn_tst_neg_data()
+  prepare_trn_data()
   # prepare_trn_early_fusion_data()
   # prepare_val_early_fusion_data()
   # train_model()
