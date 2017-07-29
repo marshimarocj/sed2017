ee797872efeaec5f67b7e71fcb7726543f54eaef
chenjia <marshimarocj@gmail.com>
2017-07-29 10:34:24 -0400

    sync

--- expr/svm.py
+++ expr/svm.py
@@ -155,9 +155,9 @@ def sample_neg_ids():
 
 def prepare_trn_tst_neg_data():
   # root_dir = '/data1/jiac/sed' # uranus
-  root_dir = '/home/jiac/data2/sed' # gpu9
+  # root_dir = '/home/jiac/data2/sed' # gpu9
   # root_dir = '/home/jiac/data/sed' # xiaojun
-  # root_dir = '/home/jiac/data/sed2017' # rocks
+  root_dir = '/home/jiac/data/sed2017' # rocks
   lst_files = [
     os.path.join(root_dir, 'dev08-1.lst'),
     os.path.join(root_dir, 'eev08-1.lst'),
@@ -169,14 +169,14 @@ def prepare_trn_tst_neg_data():
   out_files = [
     # os.path.join(root_dir, 'expr', 'c3d', 'dev08.vlad.neg.5.npz'),
     # os.path.join(root_dir, 'expr', 'c3d', 'eev08.vlad.neg.5.npz')
-    os.path.join(root_dir, 'expr', 'vgg19', 'dev08.vlad.neg.5.npz'),
-    os.path.join(root_dir, 'expr', 'vgg19', 'eev08.vlad.neg.5.npz')
-    # os.path.join(root_dir, 'expr', 'twostream', 'dev08.vlad.neg.%d.npz'%multiplier),
-    # os.path.join(root_dir, 'expr', 'twostream', 'eev08.vlad.neg.%d.npz'%multiplier)
+    # os.path.join(root_dir, 'expr', 'vgg19', 'dev08.vlad.neg.5.npz'),
+    # os.path.join(root_dir, 'expr', 'vgg19', 'eev08.vlad.neg.5.npz')
+    os.path.join(root_dir, 'expr', 'twostream', 'dev08.vlad.neg.5.npz'),
+    os.path.join(root_dir, 'expr', 'twostream', 'eev08.vlad.neg.5.npz')
   ]
   # ft_dir = os.path.join(root_dir, 'c3d', 'vlad')
-  ft_dir = os.path.join(root_dir, 'vgg19_pool5_fullres', 'vlad')
-  # ft_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'vlad')
+  # ft_dir = os.path.join(root_dir, 'vgg19_pool5_fullres', 'vlad')
+  ft_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'vlad')
 
   track_lens = [25, 50]
 
