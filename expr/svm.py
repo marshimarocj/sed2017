beca22342062d1446cf599a69d53557a29515100
chenjia <marshimarocj@gmail.com>
2017-07-31 09:05:19 -0400

    sync

--- expr/svm.py
+++ expr/svm.py
@@ -620,9 +620,12 @@ def gen_predict_script():
 
 
 def predict_on_eev():
-  root_dir = '/home/jiac/data/sed2017' # rocks
-  vlad_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'vlad')
-  model_file = os.path.join(root_dir, 'expr', 'twostream', 'svm.CellToEar.Embrace.Pointing.PersonRuns.pkl')
+  # root_dir = '/home/jiac/data/sed2017' # rocks
+  # vlad_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'vlad')
+  # model_file = os.path.join(root_dir, 'expr', 'twostream', 'svm.CellToEar.Embrace.Pointing.PersonRuns.pkl')
+  root_dir = '/data1/jiac/sed2017' # rocks
+  vlad_dir = os.path.join(root_dir, 'c3d', 'vlad')
+  model_file = os.path.join(root_dir, 'expr', 'c3d', 'svm.CellToEar.Embrace.Pointing.PersonRuns.pkl')
 
   parser = argparse.ArgumentParser()
   parser.add_argument('name')
@@ -630,8 +633,9 @@ def predict_on_eev():
 
   name = args.name
 
-  out_file = os.path.join(root_dir, 'expr', 'twostream', 'eev08_full', name + '.npz')
+  # out_file = os.path.join(root_dir, 'expr', 'twostream', 'eev08_full', name + '.npz')
   # out_file = os.path.join(root_dir, 'expr', 'twostream', 'eev08_full', name + '.raw.npz')
+  out_file = os.path.join(root_dir, 'expr', 'c3d', 'eev08_full', name + '.npz')
 
   with open(model_file) as f:
     model = cPickle.load(f)
@@ -776,7 +780,7 @@ if __name__ == '__main__':
   # train_model()
   # train_final_model()
   # val_model()
-  # predict_on_eev()
-  gen_predict_script()
+  predict_on_eev()
+  # gen_predict_script()
   # eval_full()
   # predict_on_tst2017()
