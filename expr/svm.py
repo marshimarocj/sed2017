8f19e1d755a1034d2d63f64e2d56e5eb96c76259
chenjia <marshimarocj@gmail.com>
2017-08-11 22:24:08 -0400

    sync

--- expr/svm.py
+++ expr/svm.py
@@ -8,7 +8,7 @@ import numpy as np
 from sklearn.svm import LinearSVC, SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import average_precision_score
-import liblinear.liblinear
+import liblinear
 
 import sample
 
