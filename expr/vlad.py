978f6dcb2003db014f5ecb53b8a6438c9b02c74d
chenjia <marshimarocj@gmail.com>
2017-08-11 21:36:51 -0400

    sync

--- expr/vlad.py
+++ expr/vlad.py
@@ -148,8 +148,8 @@ def encode_vlad():
   # kmeans_file = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'kmeans.center.32.pkl')
   root_dir = '/home/jiac/data/sed2017' # rocks
   # # root_dir = '/home/jiac/data/sed' # danny
-  # ft_root_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'track_group')
-  # kmeans_file = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'kmeans.center.32.pkl')
+  ft_root_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'track_group')
+  kmeans_file = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'kmeans.center.32.pkl')
   lst_files = [
     os.path.join(root_dir, 'dev08-1.lst'),
     # os.path.join(root_dir, 'eev08-1.lst'),
