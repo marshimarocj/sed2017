de8ac7814602d637c93f4537ebd94ffe99865cbd
chenjia <marshimarocj@gmail.com>
2017-08-11 21:30:42 -0400

    sync

--- expr/prepare_toi_data.py
+++ expr/prepare_toi_data.py
@@ -195,11 +195,11 @@ def shuffle_neg():
 
 
 def prepare_neg_ft():
-  # root_dir = '/data1/jiac/sed' # uranus
+  root_dir = '/data1/jiac/sed' # uranus
   # root_dir = '/home/jiac/data/sed' # xiaojun
   # root_dir = '/home/jiac/data/sed' # danny
   # root_dir = '/home/jiac/data2/sed' # gpu9
-  root_dir = '/home/jiac/data/sed2017' # rocks
+  # root_dir = '/home/jiac/data/sed2017' # rocks
   label_dir = os.path.join(root_dir, 'pseudo_label')
   track_dir = os.path.join(root_dir, 'tracking')
   # ft_root_dir = os.path.join(root_dir, 'c3d')
