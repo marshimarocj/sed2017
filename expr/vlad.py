2812550973f25c3c4a547718c0e7e66614d0839d
chenjia <marshimarocj@gmail.com>
2017-08-11 21:35:11 -0400

    sync

--- expr/vlad.py
+++ expr/vlad.py
@@ -131,9 +131,9 @@ def cluster_centers():
 
 
 def encode_vlad():
-  root_dir = '/data1/jiac/sed' # uranus
-  ft_root_dir = os.path.join(root_dir, 'c3d', 'track_group')
-  kmeans_file = os.path.join(root_dir, 'c3d', 'kmeans.center.32.pkl')
+  # root_dir = '/data1/jiac/sed' # uranus
+  # ft_root_dir = os.path.join(root_dir, 'c3d', 'track_group')
+  # kmeans_file = os.path.join(root_dir, 'c3d', 'kmeans.center.32.pkl')
   # ft_root_dir = os.path.join(root_dir, 'c3d', 'tst2017', 'track_group')
   # kmeans_file = os.path.join(root_dir, 'c3d', 'kmeans.center.32.pkl')
   # root_dir = '/home/jiac/data2/sed' # gpu9
@@ -146,7 +146,7 @@ def encode_vlad():
   # root_dir = '/home/jiac/data/sed' # xiaojun
   # ft_root_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'track_group')
   # kmeans_file = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'kmeans.center.32.pkl')
-  # root_dir = '/home/jiac/data/sed2017' # rocks
+  root_dir = '/home/jiac/data/sed2017' # rocks
   # # root_dir = '/home/jiac/data/sed' # danny
   # ft_root_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'track_group')
   # kmeans_file = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'kmeans.center.32.pkl')
@@ -155,9 +155,9 @@ def encode_vlad():
     # os.path.join(root_dir, 'eev08-1.lst'),
     # os.path.join(root_dir, '2017.refined.lst'),
   ]
-  out_dir = os.path.join(root_dir, 'c3d', 'vlad')
+  # out_dir = os.path.join(root_dir, 'c3d', 'vlad')
   # out_dir = os.path.join(root_dir, 'vgg19_pool5_fullres', 'vlad')
-  # out_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'vlad')
+  out_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'vlad')
   # out_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'tst2017', 'vlad')
   # out_dir = os.path.join(root_dir, 'c3d', 'tst2017', 'vlad')
 
