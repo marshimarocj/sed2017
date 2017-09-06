6615ebd168799edbe425814db16128af3e1350cc
chenjia <marshimarocj@gmail.com>
2017-09-06 15:07:09 -0400

    sync

---
+++ driver/netvlad.py
@@ -0,0 +1,94 @@
+import argparse
+import sys
+import os
+import datetime
+import cPickle
+sys.path.append('../')
+
+import numpy as np
+
+import model.netvlad
+
+
+def build_parser():
+  parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+
+  parser.add_argument('model_cfg_file')
+  parser.add_argument('path_cfg_file')
+  parser.add_argument('--is_train', dest='is_train', type=int, default=True)
+  parser.add_argument('--memory_fraction', dest='memory_fraction', type=float, default=1.0)
+  parser.add_argument('--wb', dest='wb', type=int, default=True)
+  parser.add_argument('--best_epoch', dest='best_epoch', type=int, default=True)
+
+  return parser
+
+
+def gen_dir_struct_info(path_cfg_file):
+  path_cfg = model.netvlad.PathCfg()
+  return common.gen_dir_struct_info(path_cfg, path_cfg_file)
+
+
+def load_and_fill_model_cfg(path_cfg, model_cfg_file):
+  model_cfg = model.netvlad.Config()
+  model_cfg.load(model_cfg_file)
+  data = np.load(path_cfg.init_weight_file)
+  model_cfg.centers = data['centers']
+  model_cfg.alpha = data['alpha']
+
+  return model_cfg
+
+
+def load_and_fill_wbmodel_cfg(path_cfg, model_cfg_file):
+  model_cfg = model.netvlad.ConfigWB()
+  model_cfg.load(model_cfg_file)
+  data = np.load(path_cfg.init_weight_file)
+  model_cfg.centers = data['centers']
+
+  return model_cfg
+
+
+if __name__ == '__main__':
+  parser = build_parser()
+  opts = parser.parse_args()
+
+  path_cfg = gen_dir_struct_info(opts.path_cfg_file)
+  if bool(opts.is_wb):
+    model_cfg = load_and_fill_wbmodel_cfg(path_cfg, model_cfg_file)
+    _model = model.netvlad.NetVladWBModel(model_cfg)
+  else:
+    model_cfg = load_and_fill_model_cfg(path_cfg, model_cfg_file)
+    _model = model.netvlad.NetVladModel(model_cfg)
+
+  if opts.is_train:
+    with open(os.path.join(path_cfg.log_dir, 'cfg.pkl'), 'w') as fout:
+      cPickle.dump(model_cfg, fout)
+      cPickle.dump(path_cfg, fout)
+      cPickle.dump(opts, fout)
+
+    trntst = model.netvlad.TrnTst(model_cfg, path_cfg, _model)
+
+    trn_reader = model.netvlad.Reader(
+      path_cfg.trn_video_lst_file, path_cfg.ft_track_group_dir, path_cfg.label_dir,
+      path_cfg.label2lid_file, model_cfg,
+      neg_lst=path_cfg.neg_lst, track_lens=path_cfg.track_lens, shuffle=True)
+    val_reader = model.netvlad.Reader(
+      path_cfg.val_video_lst_file, path_cfg.ft_track_group_dir, path_cfg.label_dir,
+      path_cfg.label2lid_file, model_cfg,
+      neg_lst=path_cfg.neg_lst, track_lens=path_cfg.track_lens, shuffle=False)
+    if path_cfg.model_file != '':
+      trntst.train(trn_reader, val_reader, memory_fraction=opts.memory_fraction, resume=True)
+    else:
+      trntst.train(trn_reader, val_reader, memory_fraction=opts.memory_fraction)
+  else:
+    path_cfg.model_file = os.path.join(path_cfg.model_dir, 'epoch-%d'%opts.best_epoch)
+    path_cfg.predict_file = os.path.join(path_cfg.output_dir, 'pred',
+      'epoch-%d.%d.%d.json'%(opts.best_epoch, opts.gen_sent_mode, model_cfg.decoder_cfg.sent_pool_size))
+    path_cfg.log_file = None
+
+    trntst = model.netvlad.TrnTst(model_cfg, path_cfg, _model)
+
+    tst_reader = model.netvlad.TstReader(
+      path_cfg.tst_video_name, path_cfg.ft_track_group_dir,
+      path_cfg.label2lid_file, model_cfg,
+      neg_lst=path_cfg.neg_lst, track_lens=path_cfg.track_lens)
+    trntst.test(tst_reader, memory_fraction=opts.memory_fraction)
