27fa5eb089d032a7d4799627e416122e8a8a91f3
chenjia <marshimarocj@gmail.com>
2017-09-08 16:15:19 -0400

    sync

--- expr/classify/svm.py
+++ expr/classify/svm.py
@@ -331,15 +331,18 @@ def prepare_tst_neg_data_with_tracklen_fixed():
 
 
 def prepare_trn_data():
-  # root_dir = '/data1/jiac/sed' # uranus
+  root_dir = '/data1/jiac/sed' # uranus
   # pos_trn_file = os.path.join(root_dir, 'expr', 'c3d', 'dev08.vlad.pos.npz')
   # neg_trn_file = os.path.join(root_dir, 'expr', 'c3d', 'dev08.vlad.neg.5.npz')
   # out_file = os.path.join(root_dir, 'expr', 'c3d', 'dev08.vlad.npz')
+  pos_trn_file = os.path.join(root_dir, 'expr', 'c3d', 'dev08.cell2ear.refine.vlad.pos.npz')
+  neg_trn_file = os.path.join(root_dir, 'expr', 'c3d', 'dev08.vlad.neg.5.npz')
+  out_file = os.path.join(root_dir, 'expr', 'c3d', 'dev08.cell2ear.refine.vlad.npz')
   # root_dir = '/home/jiac/data2/sed' # gpu9
   # pos_trn_file = os.path.join(root_dir, 'expr', 'vgg19', 'dev08.vlad.pos.npz')
   # neg_trn_file = os.path.join(root_dir, 'expr', 'vgg19', 'dev08.vlad.neg.5.npz')
   # out_file = os.path.join(root_dir, 'expr', 'vgg19', 'dev08.vlad.npz')
-  root_dir = '/home/jiac/data/sed2017' # rocks
+  # root_dir = '/home/jiac/data/sed2017' # rocks
   # pos_trn_file = os.path.join(root_dir, 'expr', 'twostream', 'dev08.vlad.pos.npz')
   # neg_trn_file = os.path.join(root_dir, 'expr', 'twostream', 'dev08.vlad.neg.5.npz')
   # out_file = os.path.join(root_dir, 'expr', 'twostream', 'dev08.vlad.npz')
@@ -349,9 +352,9 @@ def prepare_trn_data():
   # pos_trn_file = os.path.join(root_dir, 'expr', 'twostream', 'dev08.cell2ear.refine.vlad.pos.npz')
   # neg_trn_file = os.path.join(root_dir, 'expr', 'twostream', 'dev08.vlad.neg.5.npz')
   # out_file = os.path.join(root_dir, 'expr', 'twostream', 'dev08.cell2ear.refine.vlad.npz')
-  pos_trn_file = os.path.join(root_dir, 'expr', 'twostream', 'eev08.cell2ear.refine.vlad.pos.npz')
-  neg_trn_file = os.path.join(root_dir, 'expr', 'twostream', 'eev08.vlad.neg.5.npz')
-  out_file = os.path.join(root_dir, 'expr', 'twostream', 'eev08.cell2ear.refine.vlad.npz')
+  # pos_trn_file = os.path.join(root_dir, 'expr', 'twostream', 'eev08.cell2ear.refine.vlad.pos.npz')
+  # neg_trn_file = os.path.join(root_dir, 'expr', 'twostream', 'eev08.vlad.neg.5.npz')
+  # out_file = os.path.join(root_dir, 'expr', 'twostream', 'eev08.cell2ear.refine.vlad.npz')
 
   data = np.load(pos_trn_file)
   pos_fts = data['fts']
@@ -1060,12 +1063,12 @@ def eval_full():
 
 
 if __name__ == '__main__':
-  prepare_trn_tst_pos_data()
+  # prepare_trn_tst_pos_data()
   # sample_neg_ids()
   # prepare_tst_pos_data_with_tracklen_fixed()
   # prepare_tst_neg_data_with_tracklen_fixed()
   # prepare_trn_tst_neg_data()
-  # prepare_trn_data()
+  prepare_trn_data()
   # prepare_trn_txt()
   # prepare_trn_with_neg_sample()
   # prepare_trn_early_fusion_data()
