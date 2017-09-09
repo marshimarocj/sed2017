3b3185eac4d2ce97cc19b6816678c9518bba9546
chenjia <marshimarocj@gmail.com>
2017-09-09 19:52:33 -0400

    sync

--- expr/classify/netvlad.py
+++ expr/classify/netvlad.py
@@ -597,11 +597,12 @@ def gen_tst_script():
 def eval():
   root_dir = '/home/jiac/data/sed' # xiaojun
   lst_file = os.path.join(root_dir, 'meta', 'val.lst')
-  expr_name = 'netvlad.0.50'
-  # expr_name = 'netvlad.0.25'
+  # expr_name = 'netvlad.0.50'
+  expr_name = 'netvlad.0.25'
   predict_dir = os.path.join(root_dir, 'expr', 'netvlad', expr_name, 'pred')
 
-  best_epoch = 0
+  # best_epoch = 0
+  best_epoch = 9
 
   predicts = []
   labels = []
@@ -647,5 +648,5 @@ if __name__ == "__main__":
   # gen_neg_lst_for_trn()
   # neg_lst_split_by_track_len()
   # prepare_tst_files()
-  gen_tst_script()
-  # eval()
+  # gen_tst_script()
+  eval()
