894446bed08bae7dfd2fba94218c627f8c5c93fb
Jia Chen <jiac@t470>
2017-09-09 16:45:03 -0400

    sync

--- driver/netvlad.py
+++ driver/netvlad.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
     trntst = model.netvlad.TrnTst(model_cfg, path_cfg, _model)
 
     tst_reader = model.netvlad.TstReader(
-      opts.tst_video_name, path_cfg.trn_ft_track_group_dir, path_cfg.label_dir,
+      opts.tst_video_name, path_cfg.tst_ft_track_group_dir, path_cfg.label_dir,
       path_cfg.label2lid_file, model_cfg,
       track_lens=path_cfg.track_lens)
     trntst.test(tst_reader, memory_fraction=opts.memory_fraction)
