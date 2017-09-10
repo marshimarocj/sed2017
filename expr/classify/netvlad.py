a0681239852530d35bedfd27e615ec79d65b4e82
chenjia <marshimarocj@gmail.com>
2017-09-10 19:51:43 -0400

    sync

--- expr/classify/netvlad.py
+++ expr/classify/netvlad.py
@@ -597,20 +597,22 @@ def prepare_tst_files():
 
 
 def gen_tst_script():
-  root_dir = '/home/jiac/data/sed' # xiaojun
+  # root_dir = '/home/jiac/data/sed' # xiaojun
+  root_dir = '/usr0/home/jiac/data/sed' # aladdin3
   lst_file = os.path.join(root_dir, 'meta', 'val.lst')
   # expr_name = 'netvlad.0.50'
   # expr_name = 'netvlad.0.25'
   # expr_name = 'netvlad.0.25.32'
   # expr_name = 'netvlad.0.25.8'
   # expr_name = 'netvlad.l2norm.0.25.16'
-  expr_name = 'netvlad.l2norm_input.0.25.16'
+  # expr_name = 'netvlad.l2norm_input.0.25.16'
+  expr_name = 'netvlad.0.25_50'
   expr_dir = os.path.join(root_dir, 'expr', 'netvlad', expr_name)
   model_cfg_file = '%s.model.json'%expr_dir
   path_cfg_file = '%s.path.json'%expr_dir
   out_file = '../../driver/tst.sh'
 
-  gpu = 1
+  # gpu = 1
 
   val_file = os.path.join(expr_dir, 'log', 'val_metrics.pkl')
   best_epoch = select_best_epoch(val_file)
@@ -625,7 +627,7 @@ def gen_tst_script():
         names.append(name)
 
   with open(out_file, 'w') as fout:
-    fout.write('export CUDA_VISIBLE_DEVICES=%d\n'%gpu)
+    # fout.write('export CUDA_VISIBLE_DEVICES=%d\n'%gpu)
     for name in names:
       cmd = [
         'python', 'netvlad.py', 
@@ -685,7 +687,7 @@ if __name__ == "__main__":
   # class_instance_stat()
   # num_descriptor_toi_stat()
   # prepare_lst_files()
-  prepare_cfg()
+  # prepare_cfg()
   # tst_trn_reader()
   # tst_val_reader()
   # prepare_init_center_file()
@@ -695,5 +697,5 @@ if __name__ == "__main__":
   # gen_neg_lst_for_trn()
   # neg_lst_split_by_track_len()
   # prepare_tst_files()
-  # gen_tst_script()
+  gen_tst_script()
   # eval()
