dd10d7e435b695a70db3629f16f9a53bd636c05a
chenjia <marshimarocj@gmail.com>
2017-09-09 23:23:06 -0400

    sync

--- expr/classify/svm.py
+++ expr/classify/svm.py
@@ -106,15 +106,18 @@ def prepare_trn_tst_pos_data():
     # os.path.join(root_dir, 'expr', 'twostream', 'eev08.vlad.pos.npz')
     # os.path.join(root_dir, 'expr', 'twostream', 'dev08.cell2ear.refine.vlad.pos.npz'),
     # os.path.join(root_dir, 'expr', 'twostream', 'eev08.cell2ear.refine.vlad.pos.npz')
-    os.path.join(root_dir, 'expr', 'twostream', 'dev08.vlad.16.pos.npz'),
-    os.path.join(root_dir, 'expr', 'twostream', 'eev08.vlad.16.pos.npz')
+    # os.path.join(root_dir, 'expr', 'twostream', 'dev08.vlad.16.pos.npz'),
+    # os.path.join(root_dir, 'expr', 'twostream', 'eev08.vlad.16.pos.npz')
+    os.path.join(root_dir, 'expr', 'twostream', 'dev08.vlad.8.pos.npz'),
+    os.path.join(root_dir, 'expr', 'twostream', 'eev08.vlad.8.pos.npz')
   ]
   label_dir = os.path.join(root_dir, 'pseudo_label')
   # label_dir = os.path.join(root_dir, 'pseudo_label.cell2ear.refine')
   # ft_dir = os.path.join(root_dir, 'c3d', 'vlad')
   # ft_dir = os.path.join(root_dir, 'vgg19_pool5_fullres', 'vlad')
   # ft_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'vlad')
-  ft_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'vlad.16')
+  # ft_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'vlad.16')
+  ft_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'vlad.8')
 
   track_lens = [25, 50]
 
@@ -1090,7 +1093,7 @@ def eval_full():
 
 
 if __name__ == '__main__':
-  # prepare_trn_tst_pos_data()
+  prepare_trn_tst_pos_data()
   # sample_neg_ids()
   # prepare_tst_pos_data_with_tracklen_fixed()
   # prepare_tst_neg_data_with_tracklen_fixed()
@@ -1102,7 +1105,7 @@ if __name__ == '__main__':
   # prepare_val_early_fusion_data()
   # train_model()
   # train_final_model()
-  val_model()
+  # val_model()
   # predict_on_eev()
   # predict_liblinear_on_eev()
   # gen_predict_script()
