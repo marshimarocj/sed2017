3213224480225520a5486da5b6d96b537f5937a4
chenjia <marshimarocj@gmail.com>
2017-09-10 20:01:17 -0400

    sync

--- expr/classify/netvlad.py
+++ expr/classify/netvlad.py
@@ -233,11 +233,13 @@ def prepare_cfg():
   # track_lens = [25]
   # gamma = 2
 
-  out_prefix = os.path.join(out_dir, 'netvlad.0.%s.%d'%(
+  out_prefix = os.path.join(out_dir, 'netvlad.0.%s'%(
+    '_'.join([str(d) for d in track_lens])))
+  # out_prefix = os.path.join(out_dir, 'netvlad.0.%s.%d'%(
   # out_prefix = os.path.join(out_dir, 'netvlad.l2norm_output.0.%s.%d'%(
   # out_prefix = os.path.join(out_dir, 'netvlad.l2norm_input.0.%s.%d'%(
   # out_prefix = os.path.join(out_dir, 'netvlad.l2norm_input.dropout.0.%s.%d'%(
-    '_'.join([str(d) for d in track_lens]), num_center))
+    # '_'.join([str(d) for d in track_lens]), num_center))
   # out_prefix = os.path.join(out_dir, 'netvlad.focalloss.0.%s.%d.%d'%(
     # '_'.join([str(d) for d in track_lens]), num_center, gamma))
   if not os.path.exists(out_prefix):
