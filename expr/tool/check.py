e84ae365193044e89d8b664a63b7e234eb7df85b
chenjia <marshimarocj@gmail.com>
2017-09-11 20:09:46 -0400

    sync

--- expr/tool/check.py
+++ expr/tool/check.py
@@ -22,5 +22,22 @@ def missing_videos_in_preprocess():
         print name
 
 
+def mkdir_for_c3d_sync():
+  root_dir = '/home/jiac/data/sed2017' # rocks
+  lst_files = [
+    os.path.join(root_dir, 'dev08-1.lst'),
+    os.path.join(root_dir, 'eev08-1.lst'),
+  ]
+  for lst_file in lst_files:
+    with open(lst_file) as f:
+      for line in f:
+        line = line.strip()
+        name, _ = os.path.splitext(line)
+        if 'CAM4' not in name:
+          out_dir = os.path.join(root_dir, 'c3d', name)
+          os.mkdir(out_dir)
+
+
 if __name__ == '__main__':
-  missing_videos_in_preprocess()
+  # missing_videos_in_preprocess()
+  mkdir_for_c3d_sync()
