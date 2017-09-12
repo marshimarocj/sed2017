ae55885e695af71c0f2c5485c273d06500d513dd
chenjia <marshimarocj@gmail.com>
2017-09-11 22:43:29 -0400

    sync

--- expr/classify/netvlad.py
+++ expr/classify/netvlad.py
@@ -76,11 +76,12 @@ def gen_model_cfg(proto_cfg):
   }
 
 
-def gen_focal_loss_model_cfg(proto_cfg):
+def gen_focal_loss_model_cfg(proto_cfg, num_class, alphas):
   return {
     'proto': proto_cfg,
-    'num_class': 5,
+    'num_class': num_class,
     'gamma': 2,
+    'alphas': alphas,
 
     'learning_rate': 1e-4,
     'monitor_iter': 50,
@@ -221,8 +222,7 @@ def prepare_lst_files():
 def prepare_cfg():
   # root_dir = '/home/jiac/data/sed' # xiaojun
   # root_dir = '/usr0/home/jiac/data/sed' # aladdin3 
-  # root_dir = '/home/jiac/data/sed' # danny
-  root_dir = '/data1/jiac/sed' # uranus
+  root_dir = '/home/jiac/data/sed' # danny
   video_lst_files = [
     os.path.join(root_dir, 'meta', 'trn.lst'),
     # os.path.join(root_dir, 'meta', 'debug.lst'),
@@ -258,8 +258,69 @@ def prepare_cfg():
   # out_prefix = os.path.join(out_dir, 'netvlad.l2norm_input.0.%s.%d'%(
   # out_prefix = os.path.join(out_dir, 'netvlad.l2norm_input.dropout.0.%s.%d'%(
   # out_prefix = os.path.join(out_dir, 'netvlad.l2norm_input.l2norm_output.0.%s.%d'%(
-  # out_prefix = os.path.join(out_dir, 'netvlad.l2norm_input.dropin.0.%s.%d'%(
-    # '_'.join([str(d) for d in track_lens]), num_center))
+  out_prefix = os.path.join(out_dir, 'netvlad.l2norm_input.dropin.0.%s.%d'%(
+    '_'.join([str(d) for d in track_lens]), num_center))
+  if not os.path.exists(out_prefix):
+    os.mkdir(out_prefix)
+
+  proto_cfg = gen_proto_cfg(num_ft, dim_ft, num_center)
+  proto_cfg['l2_norm_input'] = True
+  # proto_cfg['l2_norm_output'] = True
+  # proto_cfg['dropin'] = True
+  model_cfg = gen_model_cfg(proto_cfg)
+  model_cfg['trn_batch_size'] = 32
+  model_cfg['tst_batch_size'] = 128
+  # model_cfg['dropout'] = True
+  model_cfg['num_epoch'] = 20
+  model_cfg_file = '%s.model.json'%out_prefix
+  with open(model_cfg_file, 'w') as fout:
+    json.dump(model_cfg, fout, indent=2)
+
+  path_cfg = {
+    'trn_video_lst_file': video_lst_files[0],
+    'val_video_lst_file': video_lst_files[1],
+    'trn_neg_lst_file': trn_neg_lst_file,
+    'trn_ft_track_group_dir': trn_ft_toi_dir, 
+    'val_ft_track_group_dir': val_ft_toi_dir, 
+    'tst_ft_track_group_dir': tst_ft_toi_dir,
+    'label_dir': label_dir,
+    'label2lid_file': label2lid_file,
+    'output_dir': out_prefix,
+    'tst_neg_lst': tst_neg_lst,
+    'track_lens': track_lens,
+    'init_weight_file': init_weight_file,
+    # 'model_file': os.path.join(out_prefix, 'model', 'epoch-9'),
+  }
+  path_cfg_file = '%s.path.json'%out_prefix
+  with open(path_cfg_file, 'w') as fout:
+    json.dump(path_cfg, fout, indent=2)
+
+
+def prepare_focalloss_cfg():
+  root_dir = '/data1/jiac/sed' # uranus
+  video_lst_files = [
+    os.path.join(root_dir, 'meta', 'trn.lst'),
+    os.path.join(root_dir, 'meta', 'val.lst'),
+  ]
+  trn_neg_lst_file = os.path.join(root_dir, 'meta', 'trn_neg.25.tfrecords.lst')
+  trn_ft_toi_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'track_group_trn_split')
+  val_ft_toi_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'track_group_val')
+  tst_ft_toi_dir = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'track_group_tst')
+  label_dir = os.path.join(root_dir, 'pseudo_label')
+  label2lid_file = os.path.join(root_dir, 'meta', 'label2lid.pkl')
+  num_center = 16
+  # num_center = 32
+  # num_center = 8
+  # init_weight_file = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'kmeans.center.%d.npz'%num_center)
+  init_weight_file = os.path.join(root_dir, 'twostream', 'feat_anet_flow_6frame', 'kmeans.center.norm.%d.npz'%num_center)
+  out_dir = os.path.join(root_dir, 'expr', 'netvlad')
+  num_ft = 100
+  dim_ft = 1024
+  tst_neg_lst = [0]
+  track_lens = [25]
+  gamma = 2
+  num_class = 5
+
   out_prefix = os.path.join(out_dir, 'netvlad.l2norm_input.focalloss.0.%s.%d.%d'%(
     '_'.join([str(d) for d in track_lens]), num_center, gamma))
   # out_prefix = os.path.join(out_dir, 'netvlad.focalloss.0.%s.%d.%d'%(
@@ -271,8 +332,12 @@ def prepare_cfg():
   proto_cfg['l2_norm_input'] = True
   # proto_cfg['l2_norm_output'] = True
   # proto_cfg['dropin'] = True
-  # model_cfg = gen_model_cfg(proto_cfg)
-  model_cfg = gen_focal_loss_model_cfg(proto_cfg)
+  neg2pos = proto_cfg['trn_neg2pos_in_batch']
+  alphas = [neg2pos] + [1./(num_class-1) for _ in range(num_class-1)]
+  alphas = np.array(alphas)
+  alphas = 1./alphas
+  alphas = self.num_class / np.sum(alphas) * alphas
+  model_cfg = gen_focal_loss_model_cfg(proto_cfg, num_class, alphas)
   model_cfg['trn_batch_size'] = 32
   model_cfg['tst_batch_size'] = 128
   model_cfg['gamma'] = gamma
@@ -731,7 +796,8 @@ if __name__ == "__main__":
   # class_instance_stat()
   # num_descriptor_toi_stat()
   # prepare_lst_files()
-  prepare_cfg()
+  # prepare_cfg()
+  prepare_focalloss_cfg()
   # tst_trn_reader()
   # tst_val_reader()
   # prepare_init_center_file()
