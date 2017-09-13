fdfd484a87444c5f01245cb332ae550e7e3b6802
Jia Chen <jiac@t470>
2017-09-13 13:16:41 -0400

    sync

--- expr/classify/attention.py
+++ expr/classify/attention.py
@@ -93,6 +93,7 @@ def prepare_cfg():
   model_cfg['trn_batch_size'] = 32
   model_cfg['tst_batch_size'] = 128
   model_cfg['num_epoch'] = 20
+  model_cfg_file = '%s.model.json'%out_prefix
   with open(model_cfg_file, 'w') as fout:
     json.dump(model_cfg, fout, indent=2)
 
