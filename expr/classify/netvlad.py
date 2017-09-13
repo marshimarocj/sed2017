164d24f7bdbe99cde390226447884997e60833ab
Jia Chen <jiac@t470>
2017-09-13 14:15:00 -0400

    sync

--- expr/classify/netvlad.py
+++ expr/classify/netvlad.py
@@ -768,14 +768,14 @@ def eval():
   # expr_name = 'netvlad.l2norm_input.dropout.0.25.16'
   # expr_name = 'netvlad.l2norm_input.l2norm_output.0.25.16'
   # expr_name = 'netvlad.l2norm_input.dropin.0.25.16'
-  # expr_name = 'netvlad.l2norm_input.focalloss.0.25.16.0.nobalance'
+  expr_name = 'netvlad.l2norm_input.focalloss.0.25.16.0.nobalance'
   # expr_name = 'netvlad.l2norm_input.focalloss.0.25.16.0'
-  expr_name = 'netvlad.l2norm_input.l2norm_output.0.25.16'
+  # expr_name = 'netvlad.l2norm_input.l2norm_output.0.25.16'
   predict_dir = os.path.join(root_dir, 'expr', 'netvlad', expr_name, 'pred')
 
   # best_epoch = 0
-  # best_epoch = 5
-  best_epoch = 10 
+  best_epoch = 5
+  # best_epoch = 10 
   # best_epoch = 9
   # best_epoch = 1
   # best_epoch = 14
@@ -825,5 +825,5 @@ if __name__ == "__main__":
   # gen_neg_lst_for_trn()
   # neg_lst_split_by_track_len()
   # prepare_tst_files()
-  gen_tst_script()
-  # eval()
+  # gen_tst_script()
+  eval()
