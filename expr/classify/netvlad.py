67bc88b373b592613b6870c5f6eef0b3623cb970
Jia Chen <jiac@t470>
2017-09-13 09:20:27 -0400

    sync

--- expr/classify/netvlad.py
+++ expr/classify/netvlad.py
@@ -752,7 +752,8 @@ def gen_tst_script():
 def eval():
   # root_dir = '/home/jiac/data/sed' # xiaojun
   # root_dir = '/usr0/home/jiac/data/sed' # aladdin3
-  root_dir = '/home/jiac/data/sed' # danny
+  # root_dir = '/home/jiac/data/sed' # danny
+  root_dir = '/data1/jiac/sed' # uranus 
   lst_file = os.path.join(root_dir, 'meta', 'val.lst')
   # expr_name = 'netvlad.0.50'
   # expr_name = 'netvlad.0.25'
@@ -762,14 +763,16 @@ def eval():
   # expr_name = 'netvlad.0.25_50'
   # expr_name = 'netvlad.l2norm_input.dropout.0.25.16'
   # expr_name = 'netvlad.l2norm_input.l2norm_output.0.25.16'
-  expr_name = 'netvlad.l2norm_input.dropin.0.25.16'
+  # expr_name = 'netvlad.l2norm_input.dropin.0.25.16'
+  expr_name = 'netvlad.l2norm_input.focalloss.0.25.16.0.nobalance'
   predict_dir = os.path.join(root_dir, 'expr', 'netvlad', expr_name, 'pred')
 
   # best_epoch = 0
   # best_epoch = 5
-  best_epoch = 2
+  # best_epoch = 2
   # best_epoch = 9
   # best_epoch = 1
+  best_epoch = 15
 
   predicts = []
   labels = []
@@ -816,5 +819,5 @@ if __name__ == "__main__":
   # gen_neg_lst_for_trn()
   # neg_lst_split_by_track_len()
   # prepare_tst_files()
-  gen_tst_script()
-  # eval()
+  # gen_tst_script()
+  eval()
