bc01a8eb987749f5bd986794a6c368f93774cb0f
Jia Chen <jiac@t470>
2017-09-18 17:25:23 -0400

    sync

--- expr/classify/netvlad.py
+++ expr/classify/netvlad.py
@@ -786,8 +786,8 @@ def gen_tst_script():
 def eval():
   # root_dir = '/home/jiac/data/sed' # xiaojun
   # root_dir = '/usr0/home/jiac/data/sed' # aladdin3
-  # root_dir = '/home/jiac/data/sed' # danny
-  root_dir = '/data1/jiac/sed' # uranus 
+  root_dir = '/home/jiac/data/sed' # danny
+  # root_dir = '/data1/jiac/sed' # uranus 
   lst_file = os.path.join(root_dir, 'meta', 'val.lst')
   # expr_name = 'netvlad.0.50'
   # expr_name = 'netvlad.0.25'
@@ -795,20 +795,22 @@ def eval():
   # expr_name = 'netvlad.0.25.8'
   # expr_name = 'netvlad.l2norm_input.0.25.16'
   # expr_name = 'netvlad.0.25_50'
-  expr_name = 'netvlad.l2norm_input.dropout.0.25.16'
+  # expr_name = 'netvlad.l2norm_input.dropout.0.25.16'
   # expr_name = 'netvlad.l2norm_input.l2norm_output.0.25.16'
   # expr_name = 'netvlad.l2norm_input.dropin.0.25.16'
   # expr_name = 'netvlad.l2norm_input.focalloss.0.25.16.0.nobalance'
   # expr_name = 'netvlad.l2norm_input.focalloss.0.25.16.0'
   # expr_name = 'netvlad.l2norm_input.focalloss.0.25.16.2.nobalance'
   # expr_name = 'netvlad.l2norm_input.l2norm_output.0.25.16'
+  expr_name = 'netvlad.c3d.l2norm_input.0.25.16'
   predict_dir = os.path.join(root_dir, 'expr', 'netvlad', expr_name, 'pred')
 
   # best_epoch = 0
   # best_epoch = 5
   # best_epoch = 2
   # best_epoch = 16
-  best_epoch = 4
+  # best_epoch = 4
+  best_epoch = 15
   # best_epoch = 10 
   # best_epoch = 9
   # best_epoch = 1
@@ -859,5 +861,5 @@ if __name__ == "__main__":
   # gen_neg_lst_for_trn()
   # neg_lst_split_by_track_len()
   # prepare_tst_files()
-  gen_tst_script()
-  # eval()
+  # gen_tst_script()
+  eval()
