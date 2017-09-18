7b59ce5dc0cc3636aea4390e95af0c561e2f5f31
Jia Chen <jiac@t470>
2017-09-18 17:21:28 -0400

    sync

--- expr/classify/netvlad.py
+++ expr/classify/netvlad.py
@@ -727,8 +727,8 @@ def prepare_tst_files():
 def gen_tst_script():
   # root_dir = '/home/jiac/data/sed' # xiaojun
   # root_dir = '/usr0/home/jiac/data/sed' # aladdin3
-  # root_dir = '/home/jiac/data/sed' # danny
-  root_dir = '/data1/jiac/sed' # uranus
+  root_dir = '/home/jiac/data/sed' # danny
+  # root_dir = '/data1/jiac/sed' # uranus
   lst_file = os.path.join(root_dir, 'meta', 'val.lst')
   # expr_name = 'netvlad.0.50'
   # expr_name = 'netvlad.0.25'
@@ -737,7 +737,7 @@ def gen_tst_script():
   # expr_name = 'netvlad.l2norm.0.25.16'
   # expr_name = 'netvlad.l2norm_input.0.25.16'
   # expr_name = 'netvlad.0.25_50'
-  expr_name = 'netvlad.l2norm_input.dropout.0.25.16'
+  # expr_name = 'netvlad.l2norm_input.dropout.0.25.16'
   # expr_name = 'netvlad.l2norm_input.l2norm_output.0.25.16'
   # expr_name = 'netvlad.l2norm_input.dropin.0.25.16'
   # expr_name = 'netvlad.l2norm_input.focalloss.0.25.16.0.nobalance'
@@ -745,6 +745,7 @@ def gen_tst_script():
   # expr_name = 'netvlad.l2norm_input.focalloss.0.25.16.2.nobalance'
   # expr_name = 'netvlad.l2norm_input.dropin.0.25.16'
   # expr_name = 'netvlad.l2norm_input.l2norm_output.0.25.16'
+  expr_name = 'netvlad.c3d.l2norm_input.0.25.16'
   expr_dir = os.path.join(root_dir, 'expr', 'netvlad', expr_name)
   model_cfg_file = '%s.model.json'%expr_dir
   path_cfg_file = '%s.path.json'%expr_dir
@@ -846,7 +847,7 @@ if __name__ == "__main__":
   # class_instance_stat()
   # num_descriptor_toi_stat()
   # prepare_lst_files()
-  prepare_cfg()
+  # prepare_cfg()
   # prepare_focalloss_cfg()
   # tst_trn_reader()
   # tst_val_reader()
@@ -857,5 +858,5 @@ if __name__ == "__main__":
   # gen_neg_lst_for_trn()
   # neg_lst_split_by_track_len()
   # prepare_tst_files()
-  # gen_tst_script()
+  gen_tst_script()
   # eval()
