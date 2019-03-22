74804158067e166f9b74b06cb2dab2af27a145a1
Jia Chen <jiac@t470>
2019-03-21 21:55:29 -0400

    sync

--- unit_test/tracking.py
+++ unit_test/tracking.py
@@ -92,8 +92,7 @@ def group_script():
 
 def generate_25fps_lst_from_5fps_lst():
   root_dir = '/usr0/home/jiac/data/sed' # aladdin1
-  # video_lst_file = os.path.join(root_dir, 'tst2017', 'dev09_preprocess.short.lst')
-  video_lst_file = os.path.join(root_dir, 'tst2017', '2017.refined.lst')
+  video_lst_file = os.path.join(root_dir, 'tst2017', 'dev09_preprocess.short.lst')
   preprocess_root_dir = os.path.join(root_dir, 'video', 'dev09', 'preprocess')
 
   with open(video_lst_file) as f:
